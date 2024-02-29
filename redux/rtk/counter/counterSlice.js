5a62a642cd035713bce211a64ac26b6b839deed7
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-02-29 09:42:28 +0600

    created counterSlice using @reduxjs/toolkit

---
+++ redux/rtk/counter/counterSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// initial state
+const initialState = {
+	count: 0,
+};
+
+// ! counter slice
+const counterSlice = createSlice({
+	name: 'counter',
+	initialState,
+	reducers: {
+		increment: (state, action) => {
+			state.count++;
+		},
+		decrement: (state, action) => {
+			state.count--;
+		},
+	},
+});
+
+export default counterSlice.reducer;
+export const { increment, decrement } = counterSlice.actions;
