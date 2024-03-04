dba3cfb9b726f459ebec2745f5ce3f6dec5eb7ac
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-04 23:25:22 +0600

    replaced the project with a video sharing platform

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,14 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import {
-	decrement,
-	dynamicDecrement,
-	dynamicIncrement,
-	increment,
-} from './features/Counter';
+import { Outlet } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 export default function App() {
-	const [inputCount, setInputCount] = useState(0);
-
-	const count = useSelector((state) => state.counter.count);
-
-	const dispatch = useDispatch();
-
-	function incrementCount() {
-		inputCount
-			? dispatch(dynamicIncrement(parseInt(inputCount)))
-			: dispatch(increment());
-
-		return setInputCount(0);
-	}
-
-	function decrementCount() {
-		inputCount
-			? dispatch(dynamicDecrement(parseInt(inputCount)))
-			: dispatch(decrement());
-
-		return setInputCount(0);
-	}
-
 	return (
-		<section className='h-[100vh] w-full bg-black text-white flex justify-center items-center'>
-			<div className='flex flex-col items-center'>
-				<h1 id='count' className='text-5xl mb-3 text-center'>
-					{count}
-				</h1>
-				<h2 className='text-3xl font-semibold'>
-					A React Redux App Example
-				</h2>
-				<input
-					id='num-input'
-					className='w-72 h-8 mt-5 px-3 bg-slate-700 text-slate-400 rounded'
-					defaultValue={inputCount}
-					onClick={(e) => setInputCount(parseInt(e.target.value))}
-				/>
-				<div className='w-fit mt-5 space-x-5'>
-					<button
-						id='increment-btn'
-						className='px-3 py-1 border-2 font-semibold rounded hover:bg-white hover:text-black duration-300'
-						onClick={incrementCount}>
-						Increment
-					</button>
-					<button
-						id='decrement-btn'
-						className='px-3 py-1 border-2 font-semibold rounded hover:bg-white hover:text-black duration-300'
-						onClick={decrementCount}>
-						Decrement
-					</button>
-				</div>
-			</div>
-		</section>
+		<>
+			<Navbar />
+			<Outlet />
+			<Footer />
+		</>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import Video from './pages/Video.jsx';
+import './index.css';
 
-import App from './App';
-import store from './app/store';
+const routes = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+			{
+				path: '/video/:id',
+				element: <Video />,
+			},
+		],
+	},
+]);
 
-ReactDOM.createRoot(document.getElementById('react')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<RouterProvider router={routes} />
 	</React.StrictMode>
 );
