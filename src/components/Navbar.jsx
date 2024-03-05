6073931fc275069f0e2c39ffc20ef415e5bf7967
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-05 12:37:37 +0600

    completed the Navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,30 @@
+import { Link } from 'react-router-dom';
+import { lwsImg, searchImg } from '../assets';
+
 export default function Navbar() {
-    return (
-        <section>
-            Navbar
-        </section>
-    );
+	return (
+		<nav className='bg-slate-100 shadow-md'>
+			<div className='max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3'>
+				<Link to='/'>
+					<img className='h-10' src={lwsImg} alt='Learn with Sumit' />
+				</Link>
+				<div className='border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200'>
+					{/* <!-- search --> */}
+					<form>
+						<input
+							className='outline-none border-none mr-2'
+							type='search'
+							name='search'
+							placeholder='Search'
+						/>
+					</form>
+					<img
+						className='inline h-4 cursor-pointer'
+						src={searchImg}
+						alt='Search'
+					/>
+				</div>
+			</div>
+		</nav>
+	);
 }
