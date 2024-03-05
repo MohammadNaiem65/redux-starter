f028d40691b3709bf6768baac50f901ed8ccf834
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-05 12:37:03 +0600

    added some needed assets

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as likeImg } from './like.svg';
+export { default as lwsImg } from './lws.svg';
+export { default as searchImg } from './search.svg';
+export { default as unlikeImg } from './unlike.svg';
