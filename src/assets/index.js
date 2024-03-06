962306edf595945c99c654594a972a1e058cf7fb
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-06 16:48:03 +0600

    updated the navbar

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,3 +2,6 @@ export { default as likeImg } from './like.svg';
 export { default as lwsImg } from './lws.svg';
 export { default as searchImg } from './search.svg';
 export { default as unlikeImg } from './unlike.svg';
+export { default as authorImg } from './author.png';
+export { default as deleteImg } from './delete.svg';
+export { default as editImg } from './edit.svg';
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,21 +8,29 @@ export default function Navbar() {
 				<Link to='/'>
 					<img className='h-10' src={lwsImg} alt='Learn with Sumit' />
 				</Link>
-				<div className='border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200'>
-					{/* <!-- search --> */}
-					<form>
-						<input
-							className='outline-none border-none mr-2'
-							type='search'
-							name='search'
-							placeholder='Search'
+
+				<div className='flex space-x-3'>
+					<div className='border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200'>
+						{/* <!-- search --> */}
+						<form>
+							<input
+								className='outline-none border-none mr-2'
+								type='search'
+								name='search'
+								placeholder='Search'
+							/>
+						</form>
+						<img
+							className='inline h-4 cursor-pointer'
+							src={searchImg}
+							alt='Search'
 						/>
-					</form>
-					<img
-						className='inline h-4 cursor-pointer'
-						src={searchImg}
-						alt='Search'
-					/>
+					</div>
+					<a
+						href='add-video.html'
+						className='inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-indigo-500'>
+						+ Add Video
+					</a>
 				</div>
 			</div>
 		</nav>
