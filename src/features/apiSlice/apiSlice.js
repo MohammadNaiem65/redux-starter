b537bdadb9dc15f0d0ac63f97a8f4f184c51ec6a
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-06 17:47:32 +0600

    created apiSlice for rtk-query

---
+++ src/features/apiSlice/apiSlice.js
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const apiSlice = createApi({
+	reducerPath: 'videoGalleryAPI',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'http://localhost:9000/',
+	}),
+	endpoints: (builder) => ({}),
+});
+
+export default apiSlice;
